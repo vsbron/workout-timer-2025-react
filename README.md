05dbeaa4017ef8a31e2de088a0576722329a969f
vsbron <vsbron@gmail.com>
2025-05-30 15:28:00 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Workout Timer
+
+Small project that I made for helping me during small workout exercises I do
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  // Returned JSX
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p>This is going to be a workout timer project</p>
+    </div>
+  );
+}
+
+export default App;
