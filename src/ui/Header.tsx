76a8a1dde796b9d48ca009069da4db42a526030a
vsbron <vsbron@gmail.com>
2025-06-01 11:56:31 +0300

    Add Logo

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -1,13 +1,23 @@
+import { useThemeContext } from "@/context/ThemeContext";
+
 import Container from "@/ui/Container";
 
+import Logo from "@/assets/images/logo.svg";
+import LogoWhite from "@/assets/images/logo-white.svg";
+
 function Header() {
+  // Getting the theme state from the context
+  const { isDarkTheme } = useThemeContext();
+
   // Returned JSX
   return (
-    <header className="text-2xl py-16 bg-stone-100 dark:stone-950 text-stone-950 dark:text-stone-50 shadow">
+    <header className="text-2xl py-10 bg-stone-100 dark:stone-950 text-stone-950 dark:text-stone-50 shadow">
       <Container>
-        <h1 className="uppercase font-semibold text-6xl text-center">
-          Workout Timer
-        </h1>
+        <img
+          src={isDarkTheme ? LogoWhite : Logo}
+          className="max-w-full w-[35rem] mx-auto"
+          title="Workout Timer"
+        />
       </Container>
     </header>
   );
