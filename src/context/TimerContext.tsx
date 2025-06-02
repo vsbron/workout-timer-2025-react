dba1183d62f35d5e5c5708d5565b44a1e0984909
vsbron <vsbron@gmail.com>
2025-06-02 22:56:03 +0300

    Greatly expand the Timer Context with new states

--- src/context/TimerContext.tsx
+++ src/context/TimerContext.tsx
@@ -7,18 +7,27 @@ import {
   type SetStateAction,
 } from "react";
 
+// Types of available phases
+type phases = "idle" | "exercise" | "break";
+
 // Interface for the context
 interface ITimerContext {
   exerciseLength: number;
   breakLength: number;
+  roundsNum: number;
+  currentPhase: phases;
   setExerciseLength: Dispatch<SetStateAction<number>>;
   setBreakLength: Dispatch<SetStateAction<number>>;
+  setRoundsNum: Dispatch<SetStateAction<number>>;
+  setCurrentPhase: Dispatch<SetStateAction<phases>>;
   resetTimer: () => void;
 }
 
 // Initial values
 const INITIAL_EXERCISE = 30;
 const INITIAL_BREAK = 15;
+const INITIAL_ROUNDS = 3;
+const INITIAL_STATE = "idle";
 
 // Create Context with undefined
 const TimerContext = createContext<ITimerContext | undefined>(undefined);
@@ -29,11 +38,15 @@ export const TimerProvider = ({ children }: { children: ReactNode }) => {
   const [exerciseLength, setExerciseLength] =
     useState<number>(INITIAL_EXERCISE);
   const [breakLength, setBreakLength] = useState<number>(INITIAL_BREAK);
+  const [roundsNum, setRoundsNum] = useState<number>(INITIAL_ROUNDS);
+  const [currentPhase, setCurrentPhase] = useState<phases>(INITIAL_STATE);
 
   // Reset timer function that initiates all the times
   const resetTimer = () => {
     setExerciseLength(INITIAL_EXERCISE);
     setBreakLength(INITIAL_BREAK);
+    setRoundsNum(INITIAL_ROUNDS);
+    setCurrentPhase(INITIAL_STATE);
   };
 
   // Returned JSX
@@ -42,8 +55,12 @@ export const TimerProvider = ({ children }: { children: ReactNode }) => {
       value={{
         exerciseLength,
         breakLength,
+        roundsNum,
+        currentPhase,
         setExerciseLength,
         setBreakLength,
+        setRoundsNum,
+        setCurrentPhase,
         resetTimer,
       }}
     >
@@ -59,15 +76,19 @@ export const useTimerContext = (): ITimerContext => {
 
   // Error if tried to use outside of Context scope
   if (!context) {
-    throw new Error("useTimerContext must be used within an AppProvider");
+    throw new Error("useTimerContext must be used within a TimerProvider");
   }
 
   // Getting the values out of context
   const {
     exerciseLength,
     breakLength,
+    roundsNum,
+    currentPhase,
     setExerciseLength,
     setBreakLength,
+    setRoundsNum,
+    setCurrentPhase,
     resetTimer,
   } = context;
 
@@ -75,8 +96,12 @@ export const useTimerContext = (): ITimerContext => {
   return {
     exerciseLength,
     breakLength,
+    roundsNum,
+    currentPhase,
     setExerciseLength,
     setBreakLength,
+    setRoundsNum,
+    setCurrentPhase,
     resetTimer,
   };
 };
