ce05dec4bde8cb13f6a2511bbf348110a6ab725f
vsbron <vsbron@gmail.com>
2025-06-02 23:00:39 +0300

    Current state is displayed above the timer

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,10 +1,24 @@
+import { useTimerContext } from "@/context/TimerContext";
 import Container from "@/ui/Container";
 
 function Timer() {
+  const { exerciseLength, breakLength, roundsNum } = useTimerContext();
+
   // Returned JSX
   return (
     <section className="mb-6">
       <Container className="text-center">
+        <div className="flex gap-8 justify-center mb-2 text-2xl">
+          <div>
+            Exercise: <b>{exerciseLength} seconds</b>
+          </div>
+          <div>
+            Break: <b>{breakLength} seconds</b>
+          </div>
+          <div>
+            Rounds: <b>{roundsNum}</b>
+          </div>
+        </div>
         <div className="text-[17rem] leading-[1] font-bold tracking-wider px-30 pt-1.5 pb-7 border border-stone-950 inline-block">
           00:00
         </div>
